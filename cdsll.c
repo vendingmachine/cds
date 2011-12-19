9457e40b7938cf54c054855b00802d21393bc96d
Ryan E Haggerty <rhaggerty@tresys.com>
2011-12-19 14:39:45 -0500

    cdsll: basic struct and ctors

---
+++ cdsll.c
@@ -0,0 +1,8 @@
+
+#include "cdsll.h"
+
+cdsll_t* cdsll_create(void)
+{
+	cdsll_t* ll;
+	return ll;
+}
---
+++ cdsll.h
@@ -0,0 +1,12 @@
+
+
+#ifndef CDSLL_H
+#define CDSLL_H
+
+typedef struct cdsll_struct cdsll_t;
+
+typedef struct cdsll_struct {
+	cdsll_t* next;
+} cdsll_t;
+
+#endif
